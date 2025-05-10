498264e1e977a2d087c3f1e90448d0760b74ff5d
NUL <noellimaa.nn@gmail.com>
2025-05-10 17:23:18 +0700

    M:glzr, kanata, powershell, wezterm;
    glzr: fixed glazewm windows rules
    kanata: change alt to one-shot-release
    powershell: add OS Exit code 7 for wezterm new pane file location
    compatibility
    wezterm: change shorcuts

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,15 @@
-oh-my-posh init pwsh --config $env:USERPROFILE\.config\oh-my-posh\nul-omp.toml | Invoke-Expression
-
 Set-Alias -Name lg -Value lazygit
 Set-Alias -Name vim -Value nvim
+
+function prompt {
+    $p = $executionContext.SessionState.Path.CurrentLocation
+    $osc7 = ""
+    if ($p.Provider.Name -eq "FileSystem") {
+        $ansi_escape = [char]27
+        $provider_path = $p.ProviderPath -Replace "\\", "/"
+        $osc7 = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}${ansi_escape}\"
+    }
+    "${osc7}PS $p$('>' * ($nestedPromptLevel + 1)) ";
+}
+
+oh-my-posh init pwsh --config $env:USERPROFILE\.config\oh-my-posh\nul-omp.toml | Invoke-Expression
