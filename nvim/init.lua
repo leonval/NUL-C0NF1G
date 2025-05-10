cf79e35ae75587ae3386bc2bd825ac83c8fd9552
leonval <noellimaa.nn@gmail.com>
2025-05-10 19:06:09 +0700

    M:kanata, nvim;
    sync configuration with windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,22 @@
+require("config.opt")
 require("config.lazy")
+-- Setup lazy.nvim
+require("lazy").setup(
+	{
+		{ import = "plugins" },
+		{ import = "plugins.lsp" },
+		{ import = "plugins.formatting" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	{
+		install = {
+			colorscheme = { "habamax" },
+		},
+		-- automatically check for plugin updates
+		checker = { enabled = true },
+		change_detection = {
+			notify = true,
+		},
+	}
+)
