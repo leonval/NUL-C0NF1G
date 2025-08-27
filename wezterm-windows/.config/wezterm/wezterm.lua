45652ec58eb10a0669d5bd44fd3e194c3a448ade
leonval <noellimaa.nn@gmail.com>
2025-08-27 14:47:27 +0700

    M: wezterm-windows;
    wezterm-windows: adjusted font fallback

--- wezterm-windows/.config/wezterm/wezterm.lua
+++ wezterm-windows/.config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ config.allow_win32_input_mode = false
 config.color_scheme = "tokyonight_night"
 config.colors = { background = "black" }
 config.default_prog = { "pwsh.exe" }
-config.font = wezterm.font_with_fallback({ "Cascadia Code", "JetBrains Mono" })
+config.font = wezterm.font_with_fallback({ "Fira Code", "Cascadia Code", "JetBrains Mono" })
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 
