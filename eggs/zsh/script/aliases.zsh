1ffad66ac5ac21284d7edd6cbde6a3f6589e2f97
leonval <noellimaa.nn@gmail.com>
2025-10-13 13:36:43 +0700

    M: niri, zsh;
    niri: add window rule for kando
    zsh: fix alias

--- eggs/zsh/script/aliases.zsh
+++ eggs/zsh/script/aliases.zsh
@@ -6,29 +6,29 @@ alias lg="lazygit"
 alias vim="nvim"
 
 # bat
-alias b = "bat"
-alias bn = "bat --number"
-alias bnl = "bat --number --line-range"
-alias bp = "bat --plain"
-alias bpl = "bat --plain --line-range"
-alias bl = "bat --line-range"
+alias b="bat"
+alias bn="bat --number"
+alias bnl="bat --number --line-range"
+alias bp="bat --plain"
+alias bpl="bat --plain --line-range"
+alias bl="bat --line-range"
 
 # eza 
-alias x = "eza --icons"
-alias xa = "eza --icons --all"
-alias xl = "eza --icons --long"
-alias xla = "eza --icons --long --all"
-alias xt = "eza --icons --tree"
-alias xta = "eza --icons --tree --all"
+alias x="eza --icons"
+alias xa="eza --icons --all"
+alias xl="eza --icons --long"
+alias xla="eza --icons --long --all"
+alias xt="eza --icons --tree"
+alias xta="eza --icons --tree --all"
 
 # yolk dotfiles
-alias ygst="yolk git status"
+alias ygs="yolk git status"
 alias ygl="yolk git log"
 alias ygaa="yolk git add -A"
 alias ygc="yolk git commit"
-alias ygP = "yolk git push"
-alias ygf = "yolk git fetch"
-alias ygp = "yolk git pull"
+alias ygP="yolk git push"
+alias ygf="yolk git fetch"
+alias ygp="yolk git pull"
 
 # laravel sail alias
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
