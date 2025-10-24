71f891dea9886d08a145e1f487a9650d69f226cd
leonval <noellimaa.nn@gmail.com>
2025-10-24 13:45:11 +0700

    M: systemd, yazi, yolk, zsh;
    systemd: add kanata systemd service for linux
    yazi: add plugins to dotfiles
    yolk: modified path for systemd
    zsh: modified new path for oh-my-posh

--- eggs/zsh/config/oh-my-posh.zsh
+++ eggs/zsh/config/oh-my-posh.zsh
@@ -1,2 +1,2 @@
 # Initialize Oh My Posh
-eval "$(oh-my-posh init zsh --config ~/.dotfiles/oh-my-posh/nul-omp.toml)"
+eval "$(oh-my-posh init zsh --config ~/.dotfiles/config/oh-my-posh/nul-omp.toml)"
